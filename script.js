409e6b858ba03ddf82340f404a3c5eb5bc180f63
halldor03 <kacoleker@gmail.com>
2022-09-04 23:26:22 +0200

    Added removing and reading functionality

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 // CONSTANTS
 const shelf = document.getElementById("shelf");
-//
-
-//REMOVE BOOK
-removeBookFromLibrary = function (e) {
-  console.log(e);
-};
+const myLibrary = [];
 //
 
 // SAMPLE BOOK
@@ -28,26 +23,32 @@ function displaySampleBook(book) {
 
   let addedBookReadStatus = document.createElement("td");
   newRow.appendChild(addedBookReadStatus);
-  addedBookReadStatus.textContent = book.isRead;
+  let readStatusButton = document.createElement("button");
+  readStatusButton.textContent = "Read";
+  addedBookReadStatus.appendChild(readStatusButton);
+  readStatusButton.addEventListener("click", () => {
+    if (readStatusButton.textContent === "Not read") {
+      readStatusButton.textContent = "Read";
+    } else readStatusButton.textContent = "Not read";
+  });
 
   let removeBook = document.createElement("td");
   newRow.appendChild(removeBook);
   let removeBookButton = document.createElement("button");
   removeBook.appendChild(removeBookButton);
   removeBookButton.textContent = "Remove";
-  removeBookButton.addEventListener("click", removeBookFromLibrary);
+  removeBookButton.addEventListener("click", () => {
+    newRow.remove();
+  });
 }
 displaySampleBook(sampleBook);
 //
 
-const myLibrary = [sampleBook];
-
 // OBJECT CONSTRUCTOR
-function book(title, author, pages, isRead) {
+function book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.isRead = isRead;
 }
 //
 
@@ -56,8 +57,7 @@ function addBookToLibrary() {
   let bookTitle = formTitle.value;
   let bookAuthor = formAuthor.value;
   let bookPages = formPages.value;
-  let bookReadStatus = "yes";
-  let newBook = new book(bookTitle, bookAuthor, bookPages, bookReadStatus);
+  let newBook = new book(bookTitle, bookAuthor, bookPages);
   myLibrary.push(newBook);
   displayBook(newBook);
 }
@@ -80,14 +80,28 @@ function displayBook(book) {
 
   let addedBookReadStatus = document.createElement("td");
   newRow.appendChild(addedBookReadStatus);
-  addedBookReadStatus.textContent = book.isRead;
+  let readStatusButton = document.createElement("button");
+  if (formRead.checked === true) {
+    readStatusButton.textContent = "Read";
+    addedBookReadStatus.appendChild(readStatusButton);
+  } else {
+    readStatusButton.textContent = "Not read";
+    addedBookReadStatus.appendChild(readStatusButton);
+  }
+  readStatusButton.addEventListener("click", () => {
+    if (readStatusButton.textContent === "Not read") {
+      readStatusButton.textContent = "Read";
+    } else readStatusButton.textContent = "Not read";
+  });
 
   let removeBook = document.createElement("td");
   newRow.appendChild(removeBook);
   let removeBookButton = document.createElement("button");
   removeBook.appendChild(removeBookButton);
   removeBookButton.textContent = "Remove";
-  removeBookButton.addEventListener("click", removeBookFromLibrary);
+  removeBookButton.addEventListener("click", () => {
+    newRow.remove();
+  });
 }
 
 addBook.addEventListener("click", addBookToLibrary);
