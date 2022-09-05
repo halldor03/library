0cfebd17ec3ea38a8d6e9a26992c636ed86cfe73
halldor03 <kacoleker@gmail.com>
2022-09-05 15:48:45 +0200

    Finished CSS

--- script.js
+++ script.js
@@ -1,48 +1,4 @@
-// CONSTANTS
-const shelf = document.getElementById("shelf");
 const myLibrary = [];
-//
-
-// SAMPLE BOOK
-const sampleBook = new book("Factfullness", "Hans Rosling", 368, "No");
-function displaySampleBook(book) {
-  let newRow = document.createElement("tr");
-  shelf.appendChild(newRow);
-
-  let addedBookTitle = document.createElement("td");
-  newRow.appendChild(addedBookTitle);
-  addedBookTitle.textContent = book.title;
-
-  let addedBookAuthor = document.createElement("td");
-  newRow.appendChild(addedBookAuthor);
-  addedBookAuthor.textContent = book.author;
-
-  let addedBookPages = document.createElement("td");
-  newRow.appendChild(addedBookPages);
-  addedBookPages.textContent = book.pages;
-
-  let addedBookReadStatus = document.createElement("td");
-  newRow.appendChild(addedBookReadStatus);
-  let readStatusButton = document.createElement("button");
-  readStatusButton.textContent = "Read";
-  addedBookReadStatus.appendChild(readStatusButton);
-  readStatusButton.addEventListener("click", () => {
-    if (readStatusButton.textContent === "Not read") {
-      readStatusButton.textContent = "Read";
-    } else readStatusButton.textContent = "Not read";
-  });
-
-  let removeBook = document.createElement("td");
-  newRow.appendChild(removeBook);
-  let removeBookButton = document.createElement("button");
-  removeBook.appendChild(removeBookButton);
-  removeBookButton.textContent = "Remove";
-  removeBookButton.addEventListener("click", () => {
-    newRow.remove();
-  });
-}
-displaySampleBook(sampleBook);
-//
 
 // OBJECT CONSTRUCTOR
 function book(title, author, pages) {
@@ -52,17 +8,28 @@ function book(title, author, pages) {
 }
 //
 
-// ADD BOOK
+// SAMPLE BOOK
+function addSampleBook(title, author, pages) {
+  const sampleBook = new book(title, author, pages);
+  myLibrary.push(sampleBook);
+  displayBook(sampleBook);
+}
+addSampleBook("Factfullness", "Hans Rosling", 368);
+//
+
+// ADD BOOK TO LIBRARY
 function addBookToLibrary() {
-  let bookTitle = formTitle.value;
-  let bookAuthor = formAuthor.value;
-  let bookPages = formPages.value;
-  let newBook = new book(bookTitle, bookAuthor, bookPages);
+  title = formTitle.value;
+  author = formAuthor.value;
+  pages = formPages.value;
+  const newBook = new book(title, author, pages);
   myLibrary.push(newBook);
   displayBook(newBook);
 }
 
 function displayBook(book) {
+  const shelf = document.getElementById("shelf");
+
   let newRow = document.createElement("tr");
   shelf.appendChild(newRow);
 
