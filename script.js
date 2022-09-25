783a48d7e1432f79fc61b93b000c4fddb8a1d871
halldor03 <kacoleker@gmail.com>
2022-09-26 00:07:34 +0200

    Started working on different approach

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function book(title, author, pages) {
 function addSampleBook(title, author, pages) {
   const sampleBook = new book(title, author, pages);
   myLibrary.push(sampleBook);
-  displayBook(sampleBook);
+  // displayBook(sampleBook);
 }
 addSampleBook("Factfullness", "Hans Rosling", 368);
 //
@@ -24,52 +24,60 @@ function addBookToLibrary() {
   pages = formPages.value;
   const newBook = new book(title, author, pages);
   myLibrary.push(newBook);
-  displayBook(newBook);
+  test();
+
+  // displayBook(newBook);
 }
 
-function displayBook(book) {
-  const shelf = document.getElementById("shelf");
-
-  let newRow = document.createElement("tr");
-  shelf.appendChild(newRow);
-
-  let addedBookTitle = document.createElement("td");
-  newRow.appendChild(addedBookTitle);
-  addedBookTitle.textContent = book.title;
-
-  let addedBookAuthor = document.createElement("td");
-  newRow.appendChild(addedBookAuthor);
-  addedBookAuthor.textContent = book.author;
-
-  let addedBookPages = document.createElement("td");
-  newRow.appendChild(addedBookPages);
-  addedBookPages.textContent = book.pages;
-
-  let addedBookReadStatus = document.createElement("td");
-  newRow.appendChild(addedBookReadStatus);
-  let readStatusButton = document.createElement("button");
-  if (formRead.checked === true) {
-    readStatusButton.textContent = "Read";
-    addedBookReadStatus.appendChild(readStatusButton);
-  } else {
-    readStatusButton.textContent = "Not read";
-    addedBookReadStatus.appendChild(readStatusButton);
+function test() {
+  for (let i = 0; i <= myLibrary.length; i++) {
+    console.log(myLibrary[i]);
   }
-  readStatusButton.addEventListener("click", () => {
-    if (readStatusButton.textContent === "Not read") {
-      readStatusButton.textContent = "Read";
-    } else readStatusButton.textContent = "Not read";
-  });
-
-  let removeBook = document.createElement("td");
-  newRow.appendChild(removeBook);
-  let removeBookButton = document.createElement("button");
-  removeBook.appendChild(removeBookButton);
-  removeBookButton.textContent = "Remove";
-  removeBookButton.addEventListener("click", () => {
-    newRow.remove();
-  });
 }
+
+// function displayBook(book) {
+//   const shelf = document.getElementById("shelf");
+
+//   let newRow = document.createElement("tr");
+//   shelf.appendChild(newRow);
+
+//   let addedBookTitle = document.createElement("td");
+//   newRow.appendChild(addedBookTitle);
+//   addedBookTitle.textContent = book.title;
+
+//   let addedBookAuthor = document.createElement("td");
+//   newRow.appendChild(addedBookAuthor);
+//   addedBookAuthor.textContent = book.author;
+
+//   let addedBookPages = document.createElement("td");
+//   newRow.appendChild(addedBookPages);
+//   addedBookPages.textContent = book.pages;
+
+//   let addedBookReadStatus = document.createElement("td");
+//   newRow.appendChild(addedBookReadStatus);
+//   let readStatusButton = document.createElement("button");
+//   if (formRead.checked === true) {
+//     readStatusButton.textContent = "Read";
+//     addedBookReadStatus.appendChild(readStatusButton);
+//   } else {
+//     readStatusButton.textContent = "Not read";
+//     addedBookReadStatus.appendChild(readStatusButton);
+//   }
+//   readStatusButton.addEventListener("click", () => {
+//     if (readStatusButton.textContent === "Not read") {
+//       readStatusButton.textContent = "Read";
+//     } else readStatusButton.textContent = "Not read";
+//   });
+
+//   let removeBook = document.createElement("td");
+//   newRow.appendChild(removeBook);
+//   let removeBookButton = document.createElement("button");
+//   removeBook.appendChild(removeBookButton);
+//   removeBookButton.textContent = "Remove";
+//   removeBookButton.addEventListener("click", () => {
+//     newRow.remove();
+//   });
+// }
 
 addBook.addEventListener("click", addBookToLibrary);
 //
